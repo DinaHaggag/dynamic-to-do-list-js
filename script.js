72e61e29b3be51f7f5cf8fdc86f68c73b4038645
DinaHaggag <dina.m.hagag@gmail.com>
2024-07-20 16:47:06 +0300

    0. Build a Dynamic To-Do List Application

---
+++ script.js
@@ -0,0 +1,40 @@
+// Setup Event Listener for Page Load:
+document.addEventListener("DOMContentLoaded", function(){
+
+  // Select DOM Elements:
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  // Create the addTask Function:
+  function addTask(){
+    const taskText = taskInput.value.trim();
+    if(taskText === ""){
+      alert("Please enter the task");
+      return;
+    }else{
+      // Task Creation and Removal:
+      let task = document.createElement("li");
+      task.textContent = taskText;
+      // Create a new button element for removing the task
+      let removeBtn = document.createElement("button");
+      removeBtn.textContent = "Remove";
+      removeBtn.classList.add("remove-btn");
+      removeBtn.addEventListener("onclick", function(){
+        taskList.removeChild(task);
+      });
+      task.appendChild(removeBtn);
+      taskList.appendChild(task);
+      // Clear the task input field 
+      taskInput.value = "" ;
+    }
+  }
+
+  // Attach Event Listeners:
+  addButton.addEventListener("onclick", addTask);
+  taskInput.addEventListener("keypress", function(event){
+    if (event.key === 'Enter') {
+      addTask();
+    }
+  })
+})
